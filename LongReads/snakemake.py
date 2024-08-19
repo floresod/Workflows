5cc71b7b1f3af870c0d6e2d417c0f86e973638a1
Daniel Flores Orozco <ibt.daniel.fo@gmail.com>
2024-08-19 16:07:04 -0500

    Code for fastqc

---
+++ LongReads/snakemake.py
@@ -0,0 +1,23 @@
+import os
+import glob 
+
+SAMPLE=glob_wildcards("RawReads/{sample}.fastq.gz")
+
+rule all: 
+    input: 
+        expand("RawQC/{sample}_fastqc.{extension}", sample=SAMPLE, extension=["html", "zip"])
+
+rule fastqc_rawreads: 
+    input: 
+        rawread="RawReads/{sample}.fastq.gz"
+    output: 
+        zip="RawQC/{sample}_fastqc.zip",
+        html="RawQC/{sample}_fastqc.html"
+    threads:
+        1
+    params:
+        path="RawQC"
+    shell:
+        """
+        fastqc {input.rawread} --threads {threads} -o {params.path}
+        """
