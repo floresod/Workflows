db73900911f4c4dc5455c891a61fb653ae32ba7d
Daniel Flores Orozco <ibt.daniel.fo@gmail.com>
2024-08-26 14:44:12 -0500

    Added kraken2 and bracken rules to snakemake file

--- LongReads/snakemake.py
+++ LongReads/snakemake.py
@@ -6,8 +6,10 @@ SAMPLE,=glob_wildcards("RawReads/{sample}.fastq.gz")
 rule all: 
     input: 
         expand("RawQC/{sample}_fastqc.{extension}", sample=SAMPLE, extension=["html", "zip"]), 
-        expand("FilteredReads/{sample}.fastq.gz", sample=SAMPLE),
-        expand("FilteredQC/{sample}_fastqc.{extension}", sample=SAMPLE, extension=["html","zip"])
+#        expand("FilteredReads/{sample}.fastq.gz", sample=SAMPLE),
+#        expand("FilteredQC/{sample}_fastqc.{extension}", sample=SAMPLE, extension=["html","zip"]),
+        expand("Kraken_report/{sample}.txt", sample=SAMPLE), 
+        expand("Bracken_report/{sample}.txt", sample=SAMPLE)
 
 
 rule fastqc_rawreads: 
@@ -25,33 +27,66 @@ rule fastqc_rawreads:
         fastqc {input.rawread} --threads {threads} -o {params.path}
         """ 
 
-rule chopper_run: 
-    input:
-        rawread="RawReads/{sample}.fastq.gz"
-    output:
-        FilteredRead="FilteredReads/{sample}.fastq.gz"
-    params:
-        threads=5
-    shell: 
-        """
-        gunzip -c {input.rawread} | \
-        chopper -q 10 -l 100 --tailcrop 10 --threads {params.threads} | \
-        gzip > {output.FilteredRead}
-        """ 
+#rule chopper_run: 
+#    input:
+#        rawread="RawReads/{sample}.fastq.gz"
+#    output:
+#        FilteredRead="FilteredReads/{sample}.fastq.gz"
+#    params:
+#        threads=5
+#    shell: 
+#        """
+#        gunzip -c {input.rawread} | \
+#        chopper -q 10 -l 100 --tailcrop 10 --threads {params.threads} | \
+#        gzip > {output.FilteredRead}
+#        """ 
 
-rule fastqc_filtered: 
-    input:
-        FilteredRead="FilteredReads/{sample}.fastq.gz"
-    output:
-        zip="FilteredQC/{sample}_fastqc.zip",
-        html="FilteredQC/{sample}_fastqc.html"
-    
+#rule fastqc_filtered: 
+#    input:
+#        FilteredRead="FilteredReads/{sample}.fastq.gz"
+#    output:
+#        zip="FilteredQC/{sample}_fastqc.zip",
+#        html="FilteredQC/{sample}_fastqc.html"
+#    
+#    params:
+#        path="FilteredQC"
+#    threads:
+#        5
+#        
+#    shell: 
+#        """
+#        fastqc {input.FilteredRead} --threads {threads} -o {params.path} 
+ #       """
+
+rule kraken2_run: 
+    input: 
+        rawread="RawReads/{sample}.fastq.gz"
     params:
-        path="FilteredQC"
-    threads:
-        5
-        
-    shell: 
+        database="../../Databases/k2_standard_08gb_20240605",
+        threads=5
+    output:
+        output="Kraken_output/{sample}.txt",
+        report="Kraken_report/{sample}.txt"
+    shell:
         """
-        fastqc {input.FilteredRead} --threads {threads} -o {params.path} 
+        kraken2 --db {params.database} --gzip-compressed {input.rawread} \
+        --output {output.output} --report {output.report} \
+        --threads {params.threads} \
+        --confidence 0.01 \
+        --use-names 
         """
+rule bracken_run:
+    input:
+        report="Kraken_report/{sample}.txt"
+    output:
+        report="Bracken_report/{sample}.txt"
+    params:
+        database="../../Databases/k2_standard_08gb_20240605",
+        length=100
+
+    shell:
+        """
+        bracken -d {params.database} -i {input.report} -o {output.report} -r {params.length}
+        """
+
+
